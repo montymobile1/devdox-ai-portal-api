c5f2441cc9f2a5d7bb2338512a8aecbc77c30b8d
Hayat Bourgi <hayat.bourgi@montyholding.com>
2025-06-19 14:30:16 +0300

    DAPA-55
    Introduced a new `APIKEY` model to manage user-specific API credentials securely. Included fields for storage, masking, and activity tracking. Updated `__init__.py` to reflect the new model in exports.

--- devdox/app/models/__init__.py
+++ devdox/app/models/__init__.py
@@ -1,5 +1,6 @@
 from .git_label import GitLabel
 from .repo import Repo
 from .user import User
+from .api_key import APIKEY
 
-__all__ = ["GitLabel", "Repo", "User"]
+__all__ = ["GitLabel", "Repo", "User", "APIKEY"]
